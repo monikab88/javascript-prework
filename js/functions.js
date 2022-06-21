f5c8c7c60d2bba99c3dba5423587ccab2a28fa5d
Monika Bielicz <bieliczm@gmail.com>
2022-06-21 23:28:29 +0200

    changes script&functions

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,6 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
-	let computerMove = `kamień`;
-	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-	let playerMove = `papier`;
-	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 }
 
 function clearMessages(){
