682a52e8e486f965d9c08d61a9910fbc50948887
Monika Bielicz <bieliczm@gmail.com>
2022-06-24 02:37:55 +0200

    changes script

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'nieznany ruch';
+let computerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
-printMessage('Twój ruch to: ' + playerMove);
-} else if (playerInput == '2'){
-	playerMove = 'papier';
-printMessage('Twój ruch to: ' + playerMove);
-} else (playerInput == '3'){
-	playerMove = 'nożyce';
-printMessage('Twój ruch to: ' + playerMove);
-}
+if(randomNumber == 1){
+  computerMove = 'kamień';
+printMessage('Mój ruch to: ' + computerMove);
+
+} else if (randomNumber == '2'){
+	computerMove = 'papier';
+
+printMessage('Mój ruch to: ' + computerMove);}
+
+else (randomNumber == '3');{
+	computerMove = 'nożyce'; 
+printMessage('Mój ruch to: ' + computerMove);}
