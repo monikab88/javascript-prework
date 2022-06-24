847890d213bdf836b10ad000a4b75ea0913e76b5
Monika Bielicz <bieliczm@gmail.com>
2022-06-24 02:10:31 +0200

    changes script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let computerMove = `kamień`;
-	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = `papier`;
-	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch to: ' + playerMove);
+else if (playerInput == '2'){
+	playerMove = 'papier';
+  }
+  printMessage('Twój ruch to: ' + playerMove);
+else (playerInput == '3'){
+	playerMove = 'nożyce';
+  }
+  printMessage('Twój ruch to: ' + playerMove);
