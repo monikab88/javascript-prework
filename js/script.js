0d1970ef5cb2fa9550d5d7949bb483fe5df0e202
Monika Bielicz <bieliczm@gmail.com>
2022-06-24 18:58:14 +0200

    changes script

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(random);
+let playerMove = getMoveName(playerInput);
 
 
 /* if(playerInput == '1'){
