979fbf433c0b9192f4c87edf66a140eec012d990
Monika Bielicz <bieliczm@gmail.com>
2022-06-24 18:53:07 +0200

    changes script

--- js/script.js
+++ js/script.js
@@ -32,7 +32,8 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(playerInput);
+let playerMove = getMoveName(random);
+
 
 /* if(playerInput == '1'){
   playerMove = 'kamień';
@@ -47,6 +48,35 @@ printMessage('Twój ruch to: ' + playerMove);
 printMessage('Twój ruch to: ' + playerMove);
 }
 */ 
+
+function displayResult(argComputerMove, argPlayerMove){
+	printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  
+	if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+	  printMessage('Ty wygrywasz!');
+	} else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+	  printMessage('Tym razem przegrywasz :(');
+	} else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+		printMessage('I mamy remis');
+	} else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+		printMessage('Remis');
+	} else if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
+		printMessage('Remis');
+	} else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+		printMessage('Ty wygrywasz!');
+	} else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+		printMessage('Ja wygrywam!');
+	} else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+		printMessage('Ja wygrywam!');
+	} else if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
+			printMessage('Remis');
+	} else {
+			printMessage('Ty wygrywasz!');
+		  }
+  }
+
+
+/*
 if( computerMove == 'kamień' && playerMove == 'papier'){
 	printMessage('Ty wygrywasz!');
   }
@@ -74,3 +104,4 @@ else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
 else {
 	printMessage('Ty wygrywasz!');
   }
+  */
