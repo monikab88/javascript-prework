60907bc6da667d6d400f8598195798955fbf09a9
Monika Bielicz <bieliczm@gmail.com>
2022-06-24 02:41:08 +0200

    changes script

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ printMessage('Mój ruch to: ' + computerMove);}
 else (randomNumber == '3');{
 	computerMove = 'nożyce'; 
 printMessage('Mój ruch to: ' + computerMove);}
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+
+printMessage('Twój ruch to: ' + playerMove);
+
+} else if (playerInput == '2'){
+	playerMove = 'papier';
+printMessage('Twój ruch to: ' + playerMove);
+} else (playerInput == '3');{
+	playerMove = 'nożyce';
+printMessage('Twój ruch to: ' + playerMove);
+}
